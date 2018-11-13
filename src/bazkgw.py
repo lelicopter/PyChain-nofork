b777a4ef50051f70a4d937064b3f5176e0c0b14f
Ковтун Владимир <C:\PROGRAMS\TheBat\Mail>
2018-11-13 13:46:32 +1000

    code format, optimizing, compatible issue solved

--- src/bazkgw.py
+++ src/bazkgw.py
@@ -4,83 +4,81 @@ from util import *
 
 def kimotogravitywell(blockindex, targetblocksspacingseconds, pastblocksmin, pastblocksmax):
 
-        debug                      = False
-        startrecord                = len(blockindex)
-	blocklastsolved	           = startrecord
-	blockreading	           = startrecord
-	pastblocksmass             = float(0)
-	pastrateactualseconds      = float(0)
-	pastratetargetseconds      = float(0)
-	pastrateadjustmentratio    = float(1)
-	pastdifficultyaverage      = float(0)
-	pastdifficultyaverageprev  = float(0)
-	eventhorizondeviation      = float(0)
-	eventhorizondeviationfast  = float(0)
-	eventhorizondeviationslow  = float(0)
-	bnproofofworklimit         = 0x1e00ffff
+    debug                      = False
+    startrecord                = len(blockindex)
+    blocklastsolved            = startrecord
+    blockreading               = startrecord
+    pastblocksmass             = float(0)
+    pastrateactualseconds      = float(0)
+    pastratetargetseconds      = float(0)
+    pastrateadjustmentratio    = float(1)
+    pastdifficultyaverage      = float(0)
+    pastdifficultyaverageprev  = float(0)
+    eventhorizondeviation      = float(0)
+    eventhorizondeviationfast  = float(0)
+    eventhorizondeviationslow  = float(0)
+    bnproofofworklimit         = 0x1e00ffff
 
-        diffiter = 0
-	while True:
-                
+    diffiter = 0
+    while True:
+        if debug is True:
+            print 'diffiter ' + str(diffiter)
+
+        diffiter += 1    
+        if (pastblocksmax > 0 and diffiter > pastblocksmax):
+            break
+        pastblocksmass += 1
+
+        if (diffiter == 1):                
+            pastdifficultyaverage = getfromblockindex(blocklastsolved, 1, blockindex)
+        else:
+            #note: this avoids a 'negative shift' exception from python
+            try:
+                pastdifficultyaverage = ((getfromblockindex(blocklastsolved, 1, blockindex) - pastdifficultyaverageprev) / diffiter) + pastdifficultyaverageprev
+            except:
                 if debug is True:
-                   print 'diffiter ' + str(diffiter)
+                    print 'hit exception.. continuing'
+                break
 
-		diffiter += 1	
-		if (pastblocksmax > 0 and diffiter > pastblocksmax):
-		    break
-		pastblocksmass += 1
+        pastdifficultyaverageprev = pastdifficultyaverage
+        pastrateactualseconds = int(getfromblockindex(blocklastsolved, 0, blockindex)) - int(getfromblockindex(blockreading, 0, blockindex))
+        pastratetargetseconds = 30 * pastblocksmass
+        pastrateadjustmentratio = float(1)        
+        if pastrateactualseconds < 0:
+            pastrateactualseconds = 0
+        if pastrateactualseconds != 0 and pastratetargetseconds != 0:
+            pastrateadjustmentratio = float(pastratetargetseconds) / float(pastrateactualseconds)
+        eventhorizondeviation = 1 + ( 0.7084 * ((pastblocksmass / 144) ** -1.228))
+        eventhorizondeviationfast = eventhorizondeviation
+        eventhorizondeviationslow = 1 / eventhorizondeviation
+        blockreading -= 1
 
-		if (diffiter == 1):			    
-		  pastdifficultyaverage = getfromblockindex(blocklastsolved, 1, blockindex)
-		else:
-                  #note: this avoids a 'negative shift' exception from python
-                  try:
-		     pastdifficultyaverage = ((getfromblockindex(blocklastsolved, 1, blockindex) - \
-                                             pastdifficultyaverageprev) / diffiter) + pastdifficultyaverageprev
-                  except:
-                     if debug is True:
-                        print 'hit exception.. continuing'
-                     break
-
-		pastdifficultyaverageprev = pastdifficultyaverage
-		pastrateactualseconds    = int(getfromblockindex(blocklastsolved, 0, blockindex)) - \
-                                           int(getfromblockindex(blockreading, 0, blockindex))
-		pastratetargetseconds    = 30 * pastblocksmass
-		pastrateadjustmentratio  = float(1)		
-		if pastrateactualseconds < 0:
-		   pastrateactualseconds = 0
-		if pastrateactualseconds != 0 and pastratetargetseconds != 0:
-		   pastrateadjustmentratio = float(pastratetargetseconds) / float(pastrateactualseconds)
-		eventhorizondeviation    = 1 + ( 0.7084 * ((pastblocksmass / 144) ** -1.228))
-		eventhorizondeviationfast = eventhorizondeviation
-		eventhorizondeviationslow = 1 / eventhorizondeviation
-		blockreading -= 1
-
-	return (pastdifficultyaverage)
+    return (pastdifficultyaverage)
 
 def getnextworkrequired(blockindex):
 
-    blockstargetspacing  = 30
-    pastblocksmin        = 28.8
-    pastblocksmax        = 403.2
+    blockstargetspacing = 30
+    pastblocksmin       = 28.8
+    pastblocksmax       = 403.2
     newbits = kimotogravitywell(blockindex, blockstargetspacing, pastblocksmin, pastblocksmax)
     return newbits
 
-def getfromblockindex(count, type, blockindex):
+def getfromblockindex(count, blocktype, blockindex):
 
     #print 'complete blockindex [' + str(blockindex) + ']'
     print 'asked for count = ' + str(count),
-    print 'of type = ' + str(type)
+    print 'of type = ' + str(blocktype)
 
     retrieved = ''
     try:
-      for num in blockindex:
-          if 'BLK' + str(count) in num:
-             retrieved = num.replace('BLK' + str(count) + ',','')
-             break
+        signstring = 'BLK%s' % count
+        for num in blockindex:
+            if signstring in num:
+                retrieved = num.replace('%s,' % signstring, '')
+                break
     except:
-      print 'blockindex error'
+        print 'blockindex error'
     if len(retrieved) > 0:
-       return retrieved.split(',')[type]
+        return retrieved.split(',')[blocktype]
     else:
-       return 'not found'
+        return 'not found'
